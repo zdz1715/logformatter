8089b08fba0ff8a3a01561cc5636fbf630eabf3d
zhudongzhuo <zdzserver@163.com>
2021-09-17 16:35:02 +0800

    增加例子

---
+++ README.md
@@ -0,0 +1,14 @@
+# logformatter
+
+简单记录golang http接口日志, 期望每次请求都会生成一条json日志，适用于所有有中间件的框架
+
+## Dependency
+- github.com/rs/zerolog
+
+## Installation
+```shell
+  go get -u github.com/zdz1715/logformatter
+```
+
+### example
+[format_test.go](./format_test.go)
--- format_test.go
+++ format_test.go
@@ -36,3 +36,41 @@ func TestLog(t *testing.T) {
 		}
 	}
 }
+
+func TestExtraLog(t *testing.T) {
+	for i := 0; i <= 1000; i++ {
+		formatter := NewContext(Context{
+			HttpRequest: HttpRequest{
+				FullUrl:       "http://c.biancheng.net/view/124.html",
+				PathInfo:      "view/124.html",
+				ClientIp:      "127.0.0.1",
+				RequestMethod: "POST",
+				RequestHeader: http.Header{
+					"Content-Type": []string{"application/json"},
+				},
+				RequestParams: "",
+				ResponseHeader: http.Header{
+					"Content-Type": []string{"application/json"},
+				},
+				ResponseBody:       "",
+				ResponseStatusCode: 200,
+			},
+			Exec: Exec{
+				ExecMs:             1.2,
+				ExecException:      nil,
+				ExecExceptionStack: string(debug.Stack()),
+			},
+		})
+
+		// 增加自定义日志结构
+		formatter.SetExtra("db_sql", []string{
+			"select * from table1",
+			"select * from table2",
+		})
+
+		//fmt.Printf("%+v", data)
+		if err := formatter.Handle(); err != nil {
+			t.Logf("err: %s", err.Error())
+		}
+	}
+}
